6b21047df07a86f80eda32ab2cddf673b6e44ec5
Kirti Swaraj <Sai@KirtiSwaraj>
2020-11-08 20:56:44 +0530

    [kirti] Add . code for finding cheapest best rated hotel for reward customer

--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -13,15 +13,32 @@ namespace HotelReservationSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hotel Reservation Program:\n");
-            //Addition of hotel with ratings into records
             HotelReservation.AddRatingsAndHotel();
-            //UC 2
-            //HotelReservation.FindCheapestHotel();
-            //UC 6
-            //HotelReservation.FindCheapestBestRatedHotel();
-            //UC 7
-            HotelReservation.FindBestRatedHotel();
-            Console.ReadLine();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nEnter:\n1.To find cheapest hotels\n2.To find best rated hotel\n3.To find cheapest best rated hotel\n4.To exit\n");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        //UC 2
+                        HotelReservation.FindCheapestHotel();
+                        break;
+                    case 2:
+                        //UC 7
+                        HotelReservation.FindBestRatedHotel();
+                        break;
+                    case 3:
+                        //UC 6
+                        HotelReservation.FindCheapestBestRatedHotel();
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
+ 
