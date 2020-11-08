8bbf17c028eb7ca3a717a1a397f5d41c8eb9ab59
Kirti Swaraj <Sai@KirtiSwaraj>
2020-11-08 12:04:33 +0530

    [kirti] add . Find cheapest hotel in a given data range

--- HotelReservationSystem/HotelReservation.cs
+++ HotelReservationSystem/HotelReservation.cs
@@ -7,6 +7,7 @@
 namespace HotelReservationSystem
 {
     using System;
+    using System.Linq;
     using System.Collections.Generic;
     using System.Text;
 
@@ -29,6 +30,32 @@ namespace HotelReservationSystem
             else
             {
                 Console.WriteLine($"Hotel {hotelName} already exists in the records\n");
+            }
+        }
+        /// <summary>
+        /// UC 2 : Finds the cheapest hotel for the date range.
+        /// </summary>
+        public static void FindCheapestHotel()
+        {
+            try
+            {
+                Console.WriteLine("Enter the check-in date(DDMMMYYYY):");
+                DateTime checkinDate = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the check-out date(DDMMMYYYY):");
+                DateTime checkoutDate = DateTime.Parse(Console.ReadLine());
+                int noOfDays = (checkoutDate - checkinDate).Days + 1;
+                Dictionary<string, int> rateRecords = new Dictionary<string, int>();
+                foreach (var v in hotelRecords)
+                {
+                    int rate = v.Value.ratePerDay * noOfDays;
+                    rateRecords.Add(v.Value.hotelName, rate);
+                }
+                var kvp = rateRecords.OrderBy(kvp => kvp.Value).First();
+                Console.WriteLine($"{kvp.Key},TotalRate:{kvp.Value}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
